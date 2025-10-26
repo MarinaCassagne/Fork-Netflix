90f29617f8f25e77a3bd729feffdeae68fab2135
Marina CASSAGNE <marina_cassagne@yahoo.fr>
2025-10-26 22:54:33 +0100

    Add initial assets and update HTML/CSS for improved layout and navigation

---
+++ TMDB.js
@@ -0,0 +1,4 @@
+const API_KEY = "2722dc9c435a979e5b05b808029b2dfe";
+
+// faire un fichier ".gitignore" à la racine du projet Fork-Netflix
+// -ajouter  .env ( = le nom du fichier) dans le fichier ".gitignore"
